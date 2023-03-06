497e2e480fe2b7548fd579c05869df4897c14b8f
Yoana <ivyo22hk@student.ju.se>
2023-03-06 16:31:24 +0100

    functionality

--- .github/lunar.lander.js
+++ .github/lunar.lander.js
@@ -1,5 +1,3 @@
-//background
-
 //stars
 let starX = [];
 let starY = [];
@@ -18,31 +16,6 @@ for (let i = 0; i < 600; i++) {
   starSize.push(size);
 }
 
-//planets
-
-let planetRed = [];
-let planetGreen = [];
-let planetBlue = [];
-let planetX = [];
-let planetY = [];
-let planetSize = [];
-
-for (let i = 0; i < 4; i++) {
-  let x = Math.floor(Math.random() * width);
-  const y = Math.floor(Math.random() * height);
-  const size = Math.random() * (200 - 40) + 40;
-  let r = Math.ceil(Math.random() * 255);
-  let g = Math.ceil(Math.random() * 255);
-  let b = Math.ceil(Math.random() * 255);
-
-  planetX.push(x);
-  planetY.push(y);
-  planetSize.push(size);
-  planetRed.push(r);
-  planetGreen.push(g);
-  planetBlue.push(b);
-}
-
 //moon
 let moonX = width / 2;
 let moonHeight = 300;
@@ -57,10 +30,34 @@ function moon(x, y, s, s2, r, g, b) {
   pop();
 }
 
+//planets
+let planetRed = [];
+let planetGreen = [];
+let planetBlue = [];
+let planetX = [];
+let planetY = [];
+let planetSize = [];
+
+for (let i = 0; i < 4; i++) {
+  let x = Math.floor(Math.random() * width);
+  const y = Math.floor(Math.random() * (moonY - moonHeight / 2 - 100) + 100);
+  const size = Math.random() * (200 - 40) + 40;
+  let r = Math.ceil(Math.random() * 255);
+  let g = Math.ceil(Math.random() * 255);
+  let b = Math.ceil(Math.random() * 255);
+
+  planetX.push(x);
+  planetY.push(y);
+  planetSize.push(size);
+  planetRed.push(r);
+  planetGreen.push(g);
+  planetBlue.push(b);
+}
+
 //space ship
 let spaceShipY = 10;
 let spaceShipX = 360;
-let velocity = 0;
+let velocity = -1;
 let acceleration = 0.2;
 let isGameActive = true;
 let spaceShipRadius = 30;
@@ -113,49 +110,34 @@ function moveUp() {
 }
 
 //the spaceship colliding with the moon
-function stopGame() {
-  let distanceX = abs(moonX - spaceShipX);
-  let distanceY = abs(moonY - spaceShipY);
-
-  if (distanceX > moonWidth / 2 + spaceShipRadius) {
-    return;
-  }
-  if (distanceY > moonHeight / 2 + spaceShipRadius) {
-    return;
-  }
-
-  if (distanceX <= moonWidth / 2) {
-    isGameActive = false;
-    return;
-  }
-  if (distanceY <= moonHeight / 2) {
-    isGameActive = false;
-    return;
-  }
-
-  let cornerDistance =
-    pow(distanceX - moonpWidth / 2, 2) + pow(distanceY - moonHeight / 2, 2);
-
-  if (cornerDistance <= pow(spaceShipRadius, 2)) {
+function shipMeetsTheMoon() {
+  if (spaceShipY + spaceShipRadius >= moonY - moonHeight / 2) {
     isGameActive = false;
   }
 }
 
-/*function stopGameWhenMeetPlanet() {
-    let distance = dist(spaceShipX, spaceShipY, planetX, planetY);
-    if (distance <= spaceShipRadius + planetSize / 2) {
+function stopCollision() {
+  for (let i = 0; i < 4; i++) {
+    /*if(planetX[index] === spaceShipX && planetY[index] === spaceShipY) {
+  isGameActive = false;
+  return;
+} }*/
+    let distance = dist(spaceShipX, spaceShipY, planetX[i], planetY[i]);
+    size = planetSize[i] / 2;
+    if (distance <= spaceShipRadius + planetSize[i] / 2) {
       isGameActive = false;
+      hitPlanet = true;
+    }
   }
-}*/
+}
 
 //start button
 let isGameStarted = false;
-
+let hitPlanet = false;
 function drawStartButton() {
   // draw the button
   fill(255);
   rect(width / 2 - 50, height / 2 - 20, 100, 40);
-
   // draw the text
   fill(0);
   textAlign(CENTER, CENTER);
@@ -172,6 +154,51 @@ function mousePressed() {
     mouseY < height / 2 + 20
   ) {
     isGameStarted = true;
+    spaceShipY = 10;
+    shipEllipseY = 25;
+    velocity = 0;
+    acceleration = 0.2;
+    isGameActive = true;
+    hitPlanet = false;
+  }
+}
+
+function startGame() {
+  if (velocity === -1) {
+    push();
+    for (let index in starX) {
+      fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+      ellipse(starX[index], starY[index], starSize[index]);
+      starAlpha[index] = starAlpha[index] + 0.02;
+    }
+    pop();
+    fill(255, 255, 255);
+    textSize(70);
+    text("Lunar Lander", width / 2, height / 2 - 110);
+  }
+}
+
+function winGame() {
+  if (hitPlanet === false && velocity < 7 && velocity >= 0) {
+    background(0, 255, 100);
+    drawStartButton();
+    fill(255, 255, 255);
+    textSize(30);
+    text("You win", width / 2, height / 2 - 70);
+    textStyle(BOLD);
+    isGameStarted = false;
+  }
+}
+
+function loseGame() {
+  if (hitPlanet === true || velocity > 7) {
+    background(255, 0, 70);
+    drawStartButton();
+    fill(255, 255, 255);
+    textSize(30);
+    text("You lose", width / 2, height / 2 - 70);
+    textStyle(BOLD);
+    isGameStarted = false;
   }
 }
 
@@ -181,18 +208,12 @@ function draw() {
 
   if (!isGameStarted) {
     drawStartButton();
+    startGame();
+    winGame();
+    loseGame();
   } else {
     drawGame();
   }
-
-  //drawing the stars
-  push();
-  for (let index in starX) {
-    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
-    ellipse(starX[index], starY[index], starSize[index]);
-    starAlpha[index] = starAlpha[index] + 0.02;
-  }
-  pop();
 
   function drawGame() {
     //drawing the stars
@@ -208,7 +229,7 @@ function draw() {
     push();
     for (let index in planetX) {
       fill(planetRed[index], planetGreen[index], planetBlue[index]);
-      ellipse(planetX[index], starY[index], planetSize[index]);
+      ellipse(planetX[index], planetY[index], planetSize[index]);
     }
     pop();
 
@@ -230,10 +251,11 @@ function draw() {
         }
         planetX[index] -= 3;
       }
-      stopGame();
+      stopCollision();
+      shipMeetsTheMoon();
     } else {
-      drawStartButton();
-      //isGameStarted = false;
+      winGame();
+      loseGame();
     }
   }
 }
